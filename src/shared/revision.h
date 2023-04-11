a91b794bb2a4e3ff91ed358cd1ab4bef2ae39928
maikale <frinci6666@abv.bg>
2023-04-11 08:58:20 +0300

    Create revision.h

---
+++ src/shared/revision.h
@@ -0,0 +1,6 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define REVISION_ID "48f92ecf280927cad8e12e03eed2688e7d0e8aba"
+ #define REVISION_DATE "2023-03-26T13:02:21+03:00"
+ #define VERSION "0.18"
+#endif // __REVISION_H__
